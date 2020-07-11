b08c1ba788ad07f28870297db15725e8d98a40f8
AB <andrew@4cast.co.za>
2020-07-11 10:57:29 +0200

    logo finished

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Prist | Gatsby & Prismic Starter`,
-        description: `A starter powered by Gatsby and Prismic to showcase portfolios and blogs.`,
-    author: `Marguerite Roth | marguerite.io`,
+    title: `4CAST | Deep Learning Agency`,
+        description: `A company dedicated to big data.`,
+    author: `Andrew`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
